d64c450504e147da153c53bc12b7831906f5ba46
031790009 <031790009@ogr.uludag.com.tr>
2023-08-09 23:30:39 +0300

    Added add functionalty to web page

--- web.py
+++ web.py
@@ -5,6 +5,7 @@ listem = functions.g_todos()
 
 def add_todo():
     todo = st.session_state["new_todos"]
+    st.session_state["new_todo"] = ""
     listem.append(todo + "\n")
     functions.w_todos(listem)
 
@@ -14,7 +15,13 @@ st.subheader("This is a todo app")
 st.write("This app to increase your productivity.")
 
 
-for ind, test in enumerate(listem):
-    st.checkbox(test, key = ind)
+for id, test in enumerate(listem):
+    st.checkbox(test, key = id)
+    if st.session_state[id]:
+        listem.pop(id)
+        functions.w_todos(listem)
+        del st.session_state[id]
+        st.experimental_rerun()
+
 
 st.text_input(label="", placeholder="Add new Todo", on_change=add_todo, key='new_todos')
